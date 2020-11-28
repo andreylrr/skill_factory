9606f5f05d9e052a74e993cd385c9ca09dc8c737
Andrey Rumyantsev <andreylrr@gmail.com>
2020-11-28 16:20:55 +0300

    Adding two home works for the section "String and Numbders" in module "Introduction to JavaScript"

---
+++ module_10/hw_4.js
@@ -0,0 +1,5 @@
+/*
+    Четвертое задание в модуле "Введение в JavaScript" в разделе "Строки и числа"
+*/
+let random_number = Math.floor(Math.random() * 100)
+alert("Random number " + random_number)
