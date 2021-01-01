ad398493ad4b210d7bcea97790a59f0ac30b240d
Andrey Rumyantsev <andreylrr@gmail.com>
2021-01-01 21:02:51 +0300

    Updating home work 3

--- module_14/hw_3.js
+++ module_14/hw_3.js
@@ -5,9 +5,14 @@
 // Ссылка на файл с отчетом
 const vUrl = 'https://my.api.mockaroo.com/revenue_2017-2019.json?key=fd36b440';
 
+// При обновлении окна прячем заготовку для таблицы
+window.onload = function () {
+    document.getElementById("div_table").hidden = true;
+}
 
 // Ф-ция обработчик события click на главной странице HTML
 function sfClick() {
+    // Проверяем был ли задан год
     if ( year_set.value === "")
     {
         alert("Выберите год, за который необходимо получить отчет");
@@ -15,13 +20,6 @@ function sfClick() {
 
     // Получаем отчет за указанный год
     const jsonReport = getReport(year_set.value);
-
-    // Создаем таблицу на HTML странице и включаем в него данные из выбранного отчета
-    //addTable(jsonReport);
-
-    const vSelect = document.body.querySelector("select");
-    document.body.querySelector("p").innerHTML = vSelect.value;
-    console.log(`${vSelect.value}`);
 }
 
 function getReport(year) {
@@ -32,16 +30,21 @@ function getReport(year) {
 
     // Создаем обработчики событий для подготовленного запроса
     xhr.onload = function () {
+
+        document.getElementById("div_table").hidden = false;
+
         if (xhr.status != 200) {
             const errorMessage = `Запрос не был обработан корректно. Статус ответа: ${xhr.status}`;
             console.log(errorMessage);
             document.body.querySelector("p").innerHTML = errorMessage;
-            console.log(`${xhr.response}`);
         }
 
         let jsonReport = JSON.parse(xhr.response);
-        for (jsonYear in jsonReport) {
-            console.log(`${jsonReport[jsonYear].year}`);
+        for(jsonYear in jsonReport)
+        {
+            if( jsonReport[jsonYear].year == year ){
+                return addTable(jsonReport[jsonYear].sales);
+            }
         }
     };
 
@@ -52,6 +55,10 @@ function getReport(year) {
     xhr.send();
 }
 
+// Обновляем таблицу за указанный год
 function addTable(objJSON){
-
+    document.getElementById("td_1").textContent = objJSON['q1'];
+    document.getElementById("td_2").textContent = objJSON['q2'];
+    document.getElementById("td_3").textContent = objJSON['q3'];
+    document.getElementById("td_4").textContent = objJSON['q4'];
 }
